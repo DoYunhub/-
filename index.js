0ab418f06902590a0fc089bbdc3aa0cb96302537
DoYun <wogns0624@naver.com>
2023-06-21 17:44:12 +0900

    여행페이지올리기

---
+++ index.js
@@ -0,0 +1,9 @@
+const intro = document.querySelector(".intro");
+
+function changeColor() {
+  style.backgroundColor = blue;
+}
+
+intro.addEventListener("click", () => {
+  intro.style.backgroundColor = "blue";
+});
